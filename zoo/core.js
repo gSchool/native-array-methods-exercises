e905ad70736f5135f23aecfa1f805500920744f4
Wes Reid <bwreid@gmail.com>
2016-01-21 22:44:11 -0700

    linting

--- zoo/core.js
+++ zoo/core.js
@@ -1,12 +1,12 @@
 var zooData = require('./data');
 
 function entryCalculator (entrants) {
-  if ( !entrants ) { return 0 };
+  if ( !entrants ) { return 0; }
   
   return Object.keys(entrants).reduce(function (total, key) {
     return total += ( entrants[key] * zooData.prices[key] );
   }, 0);
-};
+}
 
 function schedule (day) {
   function formatHours ( hours ) {
@@ -15,8 +15,8 @@ function schedule (day) {
              ( hours.close - 12 ) + 'pm';      
     } else {
       return 'CLOSED';
-    };
-  };
+    }
+  }
 
   var keys = day ? [day] : Object.keys(zooData.hours);
 
@@ -27,7 +27,7 @@ function schedule (day) {
     result[dayKey] = hoursStr;
     return result;
   }, {});
-};
+}
 
 function animalCount (query) {
   animalsAndCounts = zooData.animals.reduce(function (result, species) {
@@ -36,11 +36,11 @@ function animalCount (query) {
   }, {});
 
   return query ? animalsAndCounts[query] : animalsAndCounts;
-};
+}
 
 function animalMap (options) {
   function formatSpeciesInfo (species) {
-    if ( !options || !options.includeNames ) { return species.name };
+    if ( !options || !options.includeNames ) { return species.name; }
     
     var name = species.name;
     var result = {};
@@ -50,15 +50,14 @@ function animalMap (options) {
       result[name] = result[name].filter(function (resident) {
         return resident.sex === options.sex;
       });
-    };
+    }
 
     result[name] = result[name].map(function (animal) {
       return animal.name;
     });
 
     return result; 
-  };
-
+  }
 
   return zooData.animals.reduce(function (result, species) {
     var speciesInfo = formatSpeciesInfo(species);
@@ -67,11 +66,11 @@ function animalMap (options) {
       result[species.location].push(speciesInfo);
     } else {
       result[species.location] = [speciesInfo];
-    };
+    }
 
     return result;
   }, {});
-};
+}
 
 function animalPopularity (rating) {
   var popularityObj = zooData.animals.reduce(function (result, species) {
@@ -79,24 +78,24 @@ function animalPopularity (rating) {
       result[species.popularity].push(species.name);
     } else {
       result[species.popularity] = [species.name];
-    };
+    }
 
     return result;
   }, {});
 
   return popularityObj[rating] || popularityObj;
-};
+}
 
 function byIds (database, ids) {
-  if ( !ids ) { return [] };
-  if ( typeof ids === 'string' ) { ids = [ids] };
+  if ( !ids ) { return []; }
+  if ( typeof ids === 'string' ) { ids = [ids]; }
 
   return ids.map(function (id) {
     return database.filter(function (resource) {
       return resource.id === id;
     })[0];
   });
-};
+}
 
 function byName (database, options) {
   function constructResourceDb (resource) {
@@ -104,27 +103,27 @@ function byName (database, options) {
       return resource.residents;
     } else {
       return database;
-    };
-  };
+    }
+  }
 
   function animalFilter (resource) {
     return resource.name === options.name;
-  };
+  }
 
   function employeeFilter (resource) {
     return (resource.firstName === options.name) ||
            (resource.lastName  === options.name);
-  };
+  }
 
   function chooseFilter (resource) {
     if ( options.resources === 'animals' ) {
       return animalFilter(resource);
     } else {
       return employeeFilter(resource);
-    };
-  };
+    }
+  }
 
-  if ( !options.name ) { return {} };
+  if ( !options.name ) { return {}; }
 
   return database.reduce(function (target, resources) {
     var resourceDb = constructResourceDb(resources);
@@ -133,9 +132,9 @@ function byName (database, options) {
     if ( found ) {
       target = found;
       if ( options.resources === 'animals' ) {
-        target.species = resources.name
-      };
-    };
+        target.species = resources.name;
+      }
+    }
 
     return target;
   }, {});
@@ -143,27 +142,27 @@ function byName (database, options) {
 
 function animalsByIds (ids) {
   return byIds(zooData.animals, ids);
-};
+}
 
 function animalByName (name) {
   return byName(zooData.animals, { name: name, resources: 'animals' });
-};
+}
 
 function employeesByIds (ids) {
   return byIds(zooData.employees, ids);
-};
+}
 
 function employeeByName (name) {
   return byName(zooData.employees, { name: name, resources: 'employees '});
-};
+}
 
 function byNameOrId (nameOrId) {
   var byId = employeesByIds(nameOrId);
-  if ( byId[0] && byId[0].firstName ) { return byId };
+  if ( byId[0] && byId[0].firstName ) { return byId; }
 
   var byName = employeeByName(nameOrId);
-  if ( byName && byName.firstName ) { return [byName] };
-};
+  if ( byName && byName.firstName ) { return [byName]; }
+}
 
 function managersForEmployee (nameOrId) {
   var employee = byNameOrId(nameOrId)[0];
@@ -173,12 +172,12 @@ function managersForEmployee (nameOrId) {
   });
 
   return employee;
-};
+}
 
 function employeeCoverage (nameOrId) {
   function findEmployeeInfo () {
     return ( !nameOrId ) ? zooData.employees : byNameOrId(nameOrId);
-  };
+  }
 
   var employeeData = findEmployeeInfo();
 
@@ -191,7 +190,7 @@ function employeeCoverage (nameOrId) {
     result[employeeName] = animals;
     return result;
   }, {});
-};
+}
 
 module.exports = {
   entryCalculator: entryCalculator,
