5b9f5dcbbb782b13213fcb7e59c85a0eb4ab80df
Guillermo Moratorio <moratorio.guillermo@gmail.com>
2016-12-02 19:54:56 -0700

    completed map

--- map/core.js
+++ map/core.js
@@ -1,22 +1,44 @@
-function multiplyBy10 (array) {
- // your code here
+//Working these with strict Functional programming principles; all variables are being treated as immutable. Hence some redundancy to avoid revisiting variables
+
+function multiplyBy10(array) {
+    const by10Arr = array.map(number => {
+        return (number * 10);
+    });
+    return by10Arr;
 };
 
-function shiftRight (array) {
- // your code here
+function shiftRight(array) {
+    const shiftedArray = array.map((element, index, array) => {
+        if (index === 0) {
+            return array[array.length - 1];
+        } else {
+            return array[index - 1];
+        }
+    });
+
+    return shiftedArray;
+}
+
+function onlyVowels(array) {
+    const vowelArray = array.map((word, index) => {
+        const vowelsOnlyWord = word.replace(/[b-d]|[f-h]|[j-n]|[p-t]|[v-z]/ig, "");
+        return vowelsOnlyWord;
+    });
+    return vowelArray;
 };
 
-function onlyVowels (array) {
- // your code here
-};
-
-function doubleMatrix (array) {
- // your code here
+function doubleMatrix(array) {
+    const doubledArray = array.map((innerArray) => {
+        return innerArray.map((number) => {
+            return number * 2;
+        });
+    });
+    return doubledArray;
 };
 
 module.exports = {
-  multiplyBy10: multiplyBy10,
-  shiftRight: shiftRight,
-  onlyVowels: onlyVowels,
-  doubleMatrix: doubleMatrix
+    multiplyBy10: multiplyBy10,
+    shiftRight: shiftRight,
+    onlyVowels: onlyVowels,
+    doubleMatrix: doubleMatrix
 };
