1baf99aff3c393f84ef4ea7172d25210023922ea
Wes Reid <bwreid@gmail.com>
2017-01-27 10:20:50 -0800

    Add every and some solutions

--- every/core.js
+++ every/core.js
@@ -2,14 +2,18 @@
 // are even numbers.
 
 function allEven (input) {
-  return input;
+  return input.every(function (num) {
+    return !(num % 2);
+  });
 };
 
 // Check to see if all elements in an array
 // are of the same type.
 
 function allSameType (input) {
-  return input;
+  return input.every(function (item, index, array) {
+    return (typeof item) === (typeof array[0]);
+  });
 };
 
 // Check to see if every element in the matrix is
@@ -17,14 +21,24 @@ function allSameType (input) {
 // greater than 0.
 
 function positiveMatrix (input) {
-  return input;
+  return input.every(function (row) {
+    return Array.isArray(row) && row.every(function (num) {
+      return num > 0;
+    });
+  });
 };
 
 // Check that all items in an array are strings
 // and that they all only contain the same vowels.
 
 function allSameVowels (input) {
-  return input;
+  return input.every(function (word) {
+    var vowels = word.replace(/[^aeiou]/ig,'').split('');
+
+    return vowels.every(function (vowel, index, array) {
+      return vowel == array[0];
+    });
+  });
 };
 
 module.exports = {
--- some/core.js
+++ some/core.js
@@ -2,28 +2,40 @@
 // array are numbers greater than 10.
 
 function anyGreaterThan10 (input) {
-  return input;
+  return input.some(function (num) {
+    return num > 10;
+  });
 };
 
 // Check to see if any of the strings in
 // the array is longer than 10 characters.
 
 function longWord (input) {
-  return input;
+  return input.some(function (word) {
+    return word.length > 10;
+  });
 };
 
 // Check to see if any of the elements in
 // the matrix are true.
 
 function truePossibilities (input) {
-  return input;
+  return input.some(function (row) {
+    return row.some(function (val) {
+      return val;
+    });
+  });
 };
 
 // Check to see if 'Lost' is in
 // the phrase (using some).
 
 function lostCarcosa (input) {
-  return input;
+  return input.some(function (phrase) {
+    return phrase.split(' ').some(function (word) {
+      return word == 'Lost';
+    });
+  });
 };
 
 module.exports = {
