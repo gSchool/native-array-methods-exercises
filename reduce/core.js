2bff3469bd9654a83f1a949250b78000c90f1284
KatieAJenkins <kind2karma@gmail.com>
2017-04-26 21:18:59 -0600

    reduce 1 working

--- reduce/core.js
+++ reduce/core.js
@@ -1,6 +1,16 @@
+'use strict';
+var input = [ 10, 15, 20, 25, 30, 35 ];
+// var expected = 135
 function sum (array) {
   // your code here
-};
+  return array.reduce(function(initialValue, element){
+    console.log(initialValue);
+    console.log(element);
+    return initialValue + element;
+  });
+}
+
+console.log(sum(input));
 
 function productAll (array) {
   // your code here
